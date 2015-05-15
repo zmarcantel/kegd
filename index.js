9cccf8c1916e03d32f597707553b0879fec62e97
Zach Marcantel <zmarcantel@epochlabs.com>
2015-05-15 17:54:09 -0500

    add handlebar templating

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index');
 });
 
 var server = app.listen(3000, function () {
