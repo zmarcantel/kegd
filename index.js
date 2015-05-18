167df6ab3d4e81f11a33c16c8e4333089d04f452
Zach Marcantel <zmarcantel@epochlabs.com>
2015-05-18 15:07:40 -0500

    rename to kegd

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Kegbot listening at http://%s:%s', host, port);
+    console.log('kegd listening at http://%s:%s', host, port);
 });
 
 
